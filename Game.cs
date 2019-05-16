7988351e3ec1c92a63d1218e08c61539ccb551fd
EthanShipston <36234265+EthanShipston@users.noreply.github.com>
2019-05-15 21:50:48 -0400

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -1,4 +1,4 @@
-﻿/* Sebastion Horton
+/* Sebastion Horton
  * Tuesday, May 14, 2019
  * Class that creates the game
  */
@@ -25,7 +25,7 @@ namespace BomberMan_2._0
         Matrices matrix;
         Map map;
         public static int playerDead = 0;
-      
+
         Player player1;
         Point player1Point;
 
@@ -42,28 +42,28 @@ namespace BomberMan_2._0
             Matrices.removeBombs();
             map = new Map(c);
             player1Point = new Point(0, 0);
-            player1 = new Player(c, Brushes.DarkRed, player1Point);  //construct player1 in top left corner
+            player1 = new Player(c, 1, player1Point);  //construct player1 in top left corner
 
             player2Point = new Point(896, 512);
-            player2 = new Player(c, Brushes.DarkBlue, player2Point); //construct player2 in bottom right corner
+            player2 = new Player(c, 2, player2Point); //construct player2 in bottom right corner
 
-            players = new List<Player>() { player1, player2};
+            players = new List<Player>() { player1, player2 };
             bombs = new List<Bomb>();
 
         }
         public void updateGame()
         {
 
-            player1.updatePlayer(Key.W, Key.S, Key.A, Key.D);
-            player2.updatePlayer(Key.Up, Key.Down, Key.Left, Key.Right);
+            player1.updatePlayer(Key.W, Key.S, Key.A, Key.D, true);
+            player2.updatePlayer(Key.Up, Key.Down, Key.Left, Key.Right, false);
 
             player1.placeBomb(Key.LeftShift, players, bombs, playerDead);
             player2.placeBomb(Key.RightCtrl, players, bombs, playerDead);
-           
 
-            
-          
-            
+
+
+
+
         }
         /// <summary>
         /// Authors
@@ -71,7 +71,7 @@ namespace BomberMan_2._0
         /// if the player presses the "place" key it will check if this player already has a bomb placed that hasn't exploded.
         /// If there is no bomb placed then it runs the Bomb class' function armBomb.
         /// </summary>
-        
+
 
         /// <summary>
         /// Authors
@@ -80,13 +80,13 @@ namespace BomberMan_2._0
         /// </summary>
         public static bool isPlayerDead(Point p)
         {
-            
-                if (Matrices.bomb[(int)p.X / 64, (int)p.Y / 64] == 1)
-                {
-                    return true;
-                }
-                else
-                    return false;
+
+            if (Matrices.bomb[(int)p.X / 64, (int)p.Y / 64] == 1)
+            {
+                return true;
+            }
+            else
+                return false;
         }
     }
 }
